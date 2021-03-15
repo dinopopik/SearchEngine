7013d6854819fb20f46f23759e380941bfd2f277
Валитов Динар <DIValitov.2018@gmail.com>
2021-03-15 20:19:50 +0300

    Initial commit

---
+++ SearchEngine.Crawler/ElementExtensions.cs
@@ -0,0 +1,28 @@
+﻿using AngleSharp.Dom;
+using System.Linq;
+
+namespace SearchEngine.Crawler
+{
+	internal static class ElementExtensions
+	{
+		public static bool HasZeroOrOneChild(this IElement element)
+		{
+			while (element.ChildElementCount == 1)
+			{
+				element = element.Children.First();
+			}
+
+			return element.ChildElementCount == 0;
+		}
+
+		public static IElement Flatten(this IElement element)
+		{
+			while (element.ChildElementCount == 1)
+			{
+				element = element.Children.First();
+			}
+
+			return element;
+		}
+	}
+}
